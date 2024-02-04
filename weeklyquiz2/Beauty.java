be12210fcbe852a21d3670add4bda4fe4009ac65
stein <rla1995wnd@naver.com>
2024-02-04 23:37:02 +0900

    출력문 수정

--- weeklyquiz2/Beauty.java
+++ weeklyquiz2/Beauty.java
@@ -1,2 +1,28 @@
-package weeklyquiz2;public class Beauty {
+package weeklyquiz2;
+
+public class Beauty extends Product implements DeliveryChargeCalculator{
+    @Override
+    public int getDeliveryCharge(int weight, int price) {
+        int deliveryCharge;
+        if (weight < 3) {
+           deliveryCharge = 1000;
+        } else if (weight < 10) {
+            deliveryCharge = 5000;
+        } else {deliveryCharge = 10000;}
+
+        if (30000<= price && price<100000) {
+            deliveryCharge -= 1000;
+        } else if (price >= 100000) {
+            deliveryCharge = 0;
+        }
+        return deliveryCharge;
+    }
+
+    void output() {
+        System.out.println(this.getDeliveryCharge(this.getWeight(), this.getPrice()));
+    }
+
+    public Beauty (String name, int price, int weight) {
+        super(name, price, weight);
+    }
 }
--- weeklyquiz2/Main.java
+++ weeklyquiz2/Main.java
@@ -1,2 +1,13 @@
-package weeklyquiz2;public class Main {
+package weeklyquiz2;
+
+public class Main {
+    public static void main(String[] args) {
+        Beauty toner = new Beauty("토너", 10000, 1);
+        Grocery meat = new Grocery("한우", 99000, 3);
+        LargeAppliance oven = new LargeAppliance("오븐", 300000, 10);
+
+        toner.output();
+        meat.output();
+        oven.output();
+    }
 }
