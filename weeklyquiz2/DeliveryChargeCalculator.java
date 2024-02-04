9621169dcaae649a16beec1471be6001f157edc5
stein <rla1995wnd@naver.com>
2024-02-04 23:39:47 +0900

    주말 과제 작성

--- weeklyquiz2/DeliveryChargeCalculator.java
+++ weeklyquiz2/DeliveryChargeCalculator.java
@@ -1,2 +1,5 @@
-package weeklyquiz2;public interface DeliveryChargeCalculator {
+package weeklyquiz2;
+
+public interface DeliveryChargeCalculator {
+    int getDeliveryCharge(int weight, int price);
 }
--- weeklyquiz2/Product.java
+++ weeklyquiz2/Product.java
@@ -1,2 +1,39 @@
-package weeklyquiz2;public class Product {
+package weeklyquiz2;
+
+public class Product {
+    private String name;
+    private int price;
+    private int weight;
+
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Product (String name, int price, int weight) {
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+    }
 }
