3dd31d58b84668870e4d7ec2ce75c2b37d64ece9
stein <rla1995wnd@naver.com>
2024-02-16 21:08:05 +0900

    4주차 weekly quiz 풀이

---
+++ src/weeklyquiz4/Main.java
@@ -0,0 +1,8 @@
+package weeklyquiz4;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean answer = MatchBracket.answer("((())");
+        System.out.println(answer);
+    }
+}
---
+++ src/weeklyquiz4/MatchBracket.java
@@ -0,0 +1,26 @@
+package weeklyquiz4;
+
+import java.util.Stack;
+public class MatchBracket {
+    public static boolean answer(String s) {
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' ) {
+                stack.push("(");
+            } else {
+                if (stack.empty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        if (stack.empty()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
